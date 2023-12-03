3dada37403eddb2fef359a03e55f903a50a1ffe1
blindelectron <jump.leper@gmail.com>
2023-12-03 11:36:13 -0600

    adds elements class to retreave all pysimplegui elements easily.

--- electrode/gui/elements.py
+++ electrode/gui/elements.py
@@ -6,6 +6,12 @@ import PySimpleGUIWx as sg
 class _ElementsMeta:
 	def __getattr__(cls, name: str):
 		names=[n.__name__ for n in sg.Element.__subclasses__()]
-		correspondence=zip(names,sg.Element.__subclasses__())
+		correspondence=list(zip(names, sg.Element.__subclasses__()))
 		if name in names:
-			return 
+			return correspondence[names.index(name)][1]
+
+	def __dir__(cls):
+		return [n.__name__ for n in sg.Element.__subclasses__()]
+
+class Elements(metaclass = _ElementsMeta):
+	"""Represents the elements class."""
