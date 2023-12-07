18c0bbeee56ed1f6a56a72a86baaef8ba9fa472b
blindelectron <jump.leper@gmail.com>
2023-12-06 19:22:43 -0600

    Adds doubly linked list data type.

--- electrode/dataTypes/doublyLinkedList.py
+++ electrode/dataTypes/doublyLinkedList.py
@@ -1,11 +1,12 @@
 """
-Singly linked list for electrode.
+Doubly linked list for electrode.
 """
 
 class Node:
-	def __init__(self, data, next: Node | None = None):
+	def __init__(self, data, next: Node | None = None, prev: None|Node=None):
 		self.data=data
 		self.next=next
+		self.prev=prev
 
 class LinkedList:
 	def __init__(self, initialData: tuple):
@@ -14,8 +15,6 @@ class LinkedList:
 		self.length=0
 		for i in initialData:
 			self.append(i)
-
-
 
 	def getNode(self, index):
 		if index<0 or index>self.length: raise IndexError('Linked list index out of range.')
@@ -28,28 +27,33 @@ class LinkedList:
 			return current.next
 
 	def incert(self, data, index):
-		
 		if index==self.length:
 			self.append(data)
 			return
+		node=Node(data)
 		if index==0:
-			self.head = Node(data, next = self.head)
+			if self.head is not None:
+				node.next=self.head
+				self.head.prev=node
+			self.head=node
 			self.length+=1
 			return
 		current = self.head
-		node=Node(data)
 		for i in range(index -1):
 			current=current.next
 			if not i==index -1: continue
 			node.next=current.next
+			node.prev=current
 			current.next=node
 		self.length+=1
 
 	def append(self, data):
 		node=Node(data)
 		if self.head is None: self.head=node
-		if self.tail is not None: self.tail.next=node
-		else: self.tail=node
+		if self.tail is not None:
+			self.tail.next=node
+			node.prev=self.tail
+		self.tail=node
 		self.length+=1
 
 	def index(self, data):
@@ -68,6 +72,7 @@ class LinkedList:
 		for i in range(index -1):
 			if not  i==index -1: continue
 			current.next=current.next.next
+			current.next.prev=current
 			current=current.next
 		self.length-=1
 
