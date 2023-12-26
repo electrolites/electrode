6fc17a54874f78c67fe86a2a8505efca8c56789b
blindelectron <jump.leper@gmail.com>
2023-12-26 16:51:34 -0600

    Adds basic Window showing and hiding.

--- electrode/gui/window.py
+++ electrode/gui/window.py
@@ -1,18 +1,24 @@
 """
 Window class for electrode.
 """
-import pygame
+from pygame import display, SHOWN, HIDDEN
 
 class Window:
 	def __init__(self, title: str):
-		pygame.init()
+		display.init()
 		self._title=title
 		self.size=(640, 480)
 		self.pgRoot=None
 
 	def show(self):
-		self.pgRoot=pygame.display.set_mode(self.size)
-		pygame.display.set_caption(self._title)
+		self.pgRoot=display.set_mode(self.size, flags=SHOWN)
+		display.set_caption(self._title)
 		return self.pgRoot
 
-	
+	def hide(self):
+		self.pgRoot=display.set_mode(self.size, flags=pygame.HIDDEN)
+		return self.pgRoot
+
+	@property
+	def active(self):
+		return display.get_active()
