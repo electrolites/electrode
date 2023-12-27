87c6cea70502f582791284056ef0720f9e5a3431
blindelectron <jump.leper@gmail.com>
2023-12-27 15:16:03 -0600

    fixes several issues with the keyboard handler.

--- electrode/gui/keyboard.py
+++ electrode/gui/keyboard.py
@@ -1,19 +1,21 @@
 """
 Keyboard input class for electrode.
 """
-from pygame import event, key, KEYDOWN, KEYUP
-from keyConstants import KeyConstants
+from pygame import KEYDOWN, KEYUP
+from pygame import event as pgEvent
+from .keyConstants import KeyConstants
 
 class Keyboard:
 	def __init__(self):
-		self.pgRoot=key
 		self.heldKeys=set()
 		self.lastKeyPressed=None
 		self.lastKeyReleased=None
 		self.keyConstants=KeyConstants
 
 	def push(self):
-		events=event.get()
+		events=pgEvent.get()
+		self.lastKeyPressed=None
+		self.lastKeyReleased=None
 		for event in events:
 			if event.type==KEYUP:
 				self.heldKeys.discard(event.key)
@@ -23,7 +25,7 @@ class Keyboard:
 			if event.key not in self.heldKeys: self.lastKeyPressed=event.key
 			self.heldKeys.add(event.key)
 
-	def KEYDOWN(self, keyCode):
+	def keyDown(self, keyCode):
 		return keyCode in self.heldKeys
 
 	def keyUp(self, keyCode):
