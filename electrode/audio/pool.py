c911a7b6964ddecdd87c6542f6ca022ec1df2d85
blindelectron <jump.leper@gmail.com>
2024-01-20 09:30:38 -0600

    Pink and brown noise.

--- electrode/audio/pool.py
+++ electrode/audio/pool.py
@@ -4,7 +4,7 @@ Electrodes Sound pool class.
 import os
 import cyal
 import soundfile
-from electrode.audio.generators import sine, square, sawtooth, triangle, whiteNoise
+from electrode.audio.generators import sine, square, sawtooth, triangle, whiteNoise, brownNoise, pinkNoise
 
 class pool:
 	def __init__(self, context: cyal.context, path: str):
@@ -37,6 +37,8 @@ class pool:
 			case "sawtooth": generator = sawtooth.Sawtooth(*args, **kwargs)
 			case "triangle": generator=triangle.Triangle(*args, **kwargs)
 			case "whitenoise": generator=whiteNoise.WhiteNoise(*args, **kwargs)
+			case "brownnoise": generator=brownNoise.BrownNoise(*args, **kwargs)
+			case "pinknoise": generator = pinkNoise.PinkNoise(*args, **kwargs)
 		buffer=self.context.gen_buffer()
 		format=cyal.BufferFormat.MONO16 if generator.channels==1 else cyal.BufferFormat.STEREO16
 		buffer.set_data(generator.data, sample_rate=generator.sampleRate, format=format)
