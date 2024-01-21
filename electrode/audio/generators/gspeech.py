9156ba9eb508ab43a48639c329a3f26dc3f6cc29
blindelectron <jump.leper@gmail.com>
2024-01-21 07:59:49 -0600

    Adds System.Object[] tld options to gspeech.

--- electrode/audio/generators/gspeech.py
+++ electrode/audio/generators/gspeech.py
@@ -8,7 +8,11 @@ from electrode.audio.generators.generator import Generator
 
 class GSpeech(Generator):
 	def generate(self, text: str, **kwargs):
-		engine=gtts.gTTS(text)
+		kw={}
+		if "slow" in kwargs:  kw["slow"]= kwargs.get("slow")
+		if "language" in kwargs: kw["lang"]= kwargs.get("language")
+		if "tld" in kwargs: kw["tld"] = kwargs.get("tld")
+		engine=gtts.gTTS(text, **kw)
 		byo = BytesIO()
 		engine.write_to_fp(byo)
 		byo.seek(0)
