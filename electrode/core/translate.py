6b8a56b7f89c748103096a9aa5389c2700d8c065
blindelectron <jump.leper@gmail.com>
2024-02-16 16:33:13 -0600

    More conventions and better varyable names for translater.

--- electrode/core/translate.py
+++ electrode/core/translate.py
@@ -5,35 +5,39 @@ class Translator:
 	Translation class for electrode.
 	"""
 	def __init__(self, translationDirectory: str = ""):
+		"""
+		Initialize the translater.
+
+		:param translationDirectory: The directory in wich the translation files are found., defaults to ""
+		:type translationDirectory: str, optional
+		"""
 		self.directory = translationDirectory
 		self.cache = {}
 
-	def load(self, lang):
-		if lang in self.cache:
-			return self.cache[lang]
+	def load(self, language):
+		if language in self.cache:
+			return self.cache[language]
 
 		try:
-			with open(f"{self.directory}{lang}.json") as f:
+			with open(f"{self.directory}{language}.json") as f:
 				translation_data = json.load(f)
-				self.cache[lang] = translation_data
+				self.cache[language] = translation_data
 				return translation_data
 		except FileNotFoundError as e:
-			raise FileNotFoundError(f"Error: Translation file '{self.directory}{lang}.json' not found.")
+			raise FileNotFoundError(f"Error: Translation file '{self.directory}{language}.json' not found.")
 
-	def translate(self, key, lang, **kwargs):
-		translation_data = self.load(lang)
-		phrase_translated = translation_data.get(
-			key, f"{key} not available in {lang}")
+	def translate(self, key, language, **kwargs):
+		translation_data = self.load(language)
+		translatedPhrase = translation_data.get(
+			key, f"{key} not available in {language}")
 
 		try:
-			phrase_translated = phrase_translated.format(**kwargs)
-			return phrase_translated
+			translatedPhrase = translatedPhrase.format(**kwargs)
+			return translatedPhrase
 		except KeyError as e:
-			raise KeyError(
-				f"Error: Missing placeholder value for {e} in the translation of '{key}' ({lang}).")
+			raise KeyError(f"Error: Missing placeholder value for {e} in the translation of '{key}' ({language}).")
 		except IndexError as e:
-			raise IndexError(
-				f"Error: Index out of range in the translation of '{key}' ({lang}). Check placeholder count.")
+			raise IndexError(f"Error: Index out of range in the translation of '{key}' ({language}). Check placeholder count.")
 
 
 # Example usage:
