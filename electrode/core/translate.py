b507f2333a00eb221aa21a9c7d7ec9e1b7ff10c6
ashleygrobler04 <ashleygrobler04@gmail.com>
2024-02-16 20:04:54 +0200

    Translation

---
+++ electrode/core/translate.py
@@ -0,0 +1,42 @@
+import json
+
+
+class Translator:
+    def __init__(self):
+        self.cache = {}
+
+    def load(self, lang):
+        if lang in self.cache:
+            return self.cache[lang]
+
+        try:
+            with open(f"{lang}.json") as f:
+                translation_data = json.load(f)
+                self.cache[lang] = translation_data
+                return translation_data
+        except FileNotFoundError as e:
+            raise FileNotFoundError(
+                f"Error: Translation file '{lang}.json' not found.")
+
+    def translate(self, key, lang, **kwargs):
+        translation_data = self.load(lang)
+        phrase_translated = translation_data.get(
+            key, f"{key} not available in {lang}")
+
+        try:
+            phrase_translated = phrase_translated.format(**kwargs)
+            return phrase_translated
+        except KeyError as e:
+            raise KeyError(
+                f"Error: Missing placeholder value for {e} in the translation of '{key}' ({lang}).")
+        except IndexError as e:
+            raise IndexError(
+                f"Error: Index out of range in the translation of '{key}' ({lang}). Check placeholder count.")
+
+
+# Example usage:
+translator = Translator()
+text = translator.translate("health", "english", health=10)
+text2 = translator.translate("tired", "english")
+print(text)
+print(text2)
