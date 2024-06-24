29679b25ca9161d7e00021644a567a0581bf5d5a
blindelectron <jump.leper@gmail.com>
2024-06-23 20:25:08 -0500

    basic shader class structure.

---
+++ electrode/gui/opengl/shaders.py
@@ -0,0 +1,11 @@
+"""
+Contains the OpenGL shaders for electrode.
+"""
+from enum import Enum
+
+class Shaders(Enum):
+	"""
+	Contains Variables for the varius shaders required for electrode.
+	"""
+	vertex=""
+	fragment = ""
