cabae7e90e4af0dabda07b40f6c0d941c168b556
blindelectron <jump.leper@gmail.com>
2024-06-25 09:55:29 -0500

    events: Makes event protocols be represented as dictionaries rather than funky protocol classes that weren't being used correctly anyway.

--- electrode/events/manager.py
+++ electrode/events/manager.py
@@ -19,14 +19,14 @@ class Manager:
 		self.subscribers = defaultdict(list [Subscriber])
 		self.registered={}
 
-	async def register(self, event: str, structure: Protocol):
+	async def register(self, event: str, structure: dict):
 		"""
 		registers an event, setting up its required data.
 
 		:param event: The name of the event to register
 		:type event: str
 		:param structure: The required data for the event.
-		:type structure: Protocol
+		:type structure: dict
 		"""
 		if event in self.registered.keys(): raise eventExistsError(event, self.registered[event], structure)
 		self.registered[event] = structure
@@ -40,19 +40,19 @@ class Manager:
 		:param callBack: The function that is awaited upon the execution of the event.
 		:type callBack: Coroutine
 		:param requirements: The data that is required for this callBack
-		:type requirements: dict
+		:type requirements: dict, optional
 		"""
 		if event not in self.registered.keys(): raise eventMissingError(f'The event {event} does not exist.', event)
 		structure = self.registered[event]
 		for k in requirements.keys():
-			if k in structure.__annotations__: continue
+			if k in structure.keys(): continue
 			raise invalidRequirementsError(f'The requirement {k} is not valid because it is not with in the registered structure for the {event} event.', requirements)
 		subscriber = Subscriber(callBack, requirements)
 		self.subscribers[event].append(subscriber)
 
 	async def unregister(self, event: str):
 		"""
-		unregisters an event, the event and all it's callBacks.
+		Unregisters an event and all it's callBacks.
 
 		:param event: The name of the event to unregister
 		:type event: str
@@ -72,10 +72,10 @@ class Manager:
 		"""
 		if event not in self.registered.keys(): raise eventMissingError(f'The event {event} does not exist.', event)
 		for key,value in data.items():
-			if key not in self.registered[event].__annotations__: raise invalidEventError(f'The event {event} did not matche its registered protocol. It got a value for {key}, which does not exist in its registered protocol.', event)
-			if self.registered[event].__annotations__[key] is Any: continue
-			if isinstance(value, self.registered[event].__annotations__[key]): continue
-			raise invalidEventError(f'The event {event} did not matche its registered protocol. It was expecting the key {key} to be of type {self.registered[event].__annotations__[key]} but it was of type {type(value)}', event)
+			if key not in self.registered[event].keys(): raise invalidEventError(f'The event {event} did not matche its registered protocol. It got a value for {key}, which does not exist in its registered protocol.', event)
+			if self.registered[event][key] is Any: continue
+			if isinstance(value, self.registered[event][key]): continue
+			raise invalidEventError(f'The event {event} did not matche its registered protocol. It was expecting the key {key} to be of type {self.registered[event][key]} but it was of type {type(value)}', event)
 		if event not in self.subscribers.keys(): return
 		await asyncio.gather(*[e.Callback(data) for e in self.subscribers[event] if e.requirements.items() in data.items()])
 
